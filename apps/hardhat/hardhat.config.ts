77a4efaa92d8e261ba5240d9746af25f9058b085
Ken Mondragon <136342229+antuK3n@users.noreply.github.com>
2025-07-28 06:29:17 +0800

    Update hardhat.config.ts

--- apps/hardhat/hardhat.config.ts
+++ apps/hardhat/hardhat.config.ts
@@ -9,7 +9,7 @@ const config: HardhatUserConfig = {
       url: "https://1rpc.io/base",
       chainId: 8453,
       accounts: [
-        "e3600fa55266b44dc706d58c8b13a770249f7d982168c5eaabc5da062b352804",
+        "",
       ], // Add your private key here if needed
     },
   },
